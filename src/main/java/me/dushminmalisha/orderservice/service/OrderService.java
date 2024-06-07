15e9923cea3414fe7f7646e858d6e2bf0fcc2f87
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-06-07 23:42:43 +0530

    add transactional annotation

--- src/main/java/me/dushminmalisha/orderservice/service/OrderService.java
+++ src/main/java/me/dushminmalisha/orderservice/service/OrderService.java
@@ -7,12 +7,14 @@ import me.dushminmalisha.orderservice.model.Order;
 import me.dushminmalisha.orderservice.model.OrderLineItems;
 import me.dushminmalisha.orderservice.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
 
